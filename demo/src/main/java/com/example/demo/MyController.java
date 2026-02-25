7b3ec7e6fe44e75705b009a29bbffdd1fc11506c
jhengwh <chrishu.twn@gmail.com>
2026-02-25 17:26:21 +0800

    Practice04: Add Value annotation to assign value to the Bean variable

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @Autowired
-    @Qualifier("hpPrinter")
+    @Qualifier("canonPrinter")
     private Printer printer;
 
     // Run when a request is received
